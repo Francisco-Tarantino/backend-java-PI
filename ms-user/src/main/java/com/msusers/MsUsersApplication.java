f2f90699cbcf255bd96008e5f80eae7625c23079
Francisco <frantarantino2018@gmail.com>
2024-05-05 19:32:45 -0300

    ms-users

---
+++ ms-user/src/main/java/com/msusers/MsUsersApplication.java
@@ -0,0 +1,15 @@
+package com.msusers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@EnableEurekaClient
+@SpringBootApplication
+public class MsUsersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsUsersApplication.class, args);
+    }
+
+}
